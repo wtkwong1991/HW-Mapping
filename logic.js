f329576d68144a1f71175b24cd32a3f23efa285e
wtkwong1991 <47372271+wtkwong1991@users.noreply.github.com>
2019-06-15 10:30:04 -0500

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,6 +1,5 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
@@ -120,14 +119,12 @@ function createMap(earthquakes) {
 
   legend.onAdd = function(myMap) {
     var div = L.DomUtil.create('div', 'info legend'),   
-        colors = [0, 1, 2, 3, 4, 5],
-        labels = [];
+        colors = [0, 1, 2, 3, 4, 5];
 
-    for (var i = 0; i < colors.length; i++) {
-      div.innerHTML +=
-      '<i style="background:' + getColor(colors[i] + 1) + '"></i> ' +
-      colors[i] + (colors[i + 1] ? '&ndash;' + colors[i + 1] + '<br' : '+');
-    }
+        for (var i = 0; i < colors.length; i++) {
+          div.innerHTML +='<i style="background:' + getColor(colors[i] + 1) + '"></i> ' +
+          colors[i] + (colors[i + 1] ? '&ndash;' + colors[i + 1] + '<br>' : '+');
+        }
 
     return div;
   };
